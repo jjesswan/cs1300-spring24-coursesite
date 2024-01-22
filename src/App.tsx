0245b9c7d773f054dcc25a84b1d89c83ca5a9aae
jjesswan <jessica_wan@brown.edu>
2024-01-22 17:20:46 -0500

    add service worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import * as React from "react";
-import { ChakraProvider, theme } from "@chakra-ui/react";
-import Home from "./components/Home";
-import "./styles/global.css";
+import * as React from "react"
+import {
+  ChakraProvider,
+  theme,
+} from "@chakra-ui/react"
+import Home from "./components/Home"
+import Navbar from "./components/navigation/Navbar"
+import "./styles/global.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import customTheme from "./styles/theme";
-import Navigation from "./components/navigation/Navigation";
+import customTheme from "./styles/theme"
+import Hamburger from "./components/navigation/Hamburger"
+import SideNav from "./components/navigation/SideNav"
+import Navigation from "./components/navigation/Navigation"
 
-export default function App() {
+ 
+
+export default function App(){
   return (
     <ChakraProvider theme={customTheme}>
       <BrowserRouter>
+        {/* <Navbar /> */}
         <Navigation />
-        <Home />
+        <Home/>
       </BrowserRouter>
     </ChakraProvider>
   );
